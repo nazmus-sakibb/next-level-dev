5fe6db3ff5981464a2371cd9b80ecce718044b72
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-02 17:45:04 +0600

    primitive type is typescript

---
+++ module-1/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const course = 'next level development';
+console.log(course);
---
+++ module-1/src/index.ts
@@ -0,0 +1,5 @@
+let course: string = "next level development";
+course = 'Web Development';
+let rollNumber: number = 123;
+rollNumber = 566;
+const isAdmin: boolean = true;
