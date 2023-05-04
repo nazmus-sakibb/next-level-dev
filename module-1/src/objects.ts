7871babcc9a1c5063458925dd21064c284965917
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-04 18:26:07 +0600

    object, literal types and optional types

--- module-1/src/objects.ts
+++ module-1/src/objects.ts
@@ -1,6 +1,14 @@
-const user = {
+const user: {
+    readonly company:string,
+    name:string,
+    age:number, 
+    isMarried: boolean,
+    car?:string
+} = {
+    company: 'Netlify',
     name: 'Montu Mia',
     age: 67,
     isMarried: false,
-    car: 'toyota'
 }
+
+// user.company='Netlify bangladesh'
