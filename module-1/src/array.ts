ef06c1321fb7caa3939e61893984c8e0f0455dbc
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-04 18:12:05 +0600

    array and tuples in typescript

---
+++ module-1/src/array.ts
@@ -0,0 +1,13 @@
+const names = ['abul', 'kabul', 'babul', 45, false];
+
+// names.push(56, true, 'null', null);
+
+// let user: [number, boolean] = [45, true];
+
+// user[1] = false;
+// user[0] = 89;
+
+
+const address: [string, boolean] = ['Mohakhali', true];
+
+
