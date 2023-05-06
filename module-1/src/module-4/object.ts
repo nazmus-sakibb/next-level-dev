d0d6acbb953801a85f8d171ea6df8d6faec92b21
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-06 14:34:20 +0600

    class, object, parameter properties

--- module-1/src/module-4/object.ts
+++ module-1/src/module-4/object.ts
@@ -1,1 +1,22 @@
-class Animal
+class Animal {
+//   public name: string;
+//   public species: string;
+//   public sound: string;
+
+  // parameter properties
+  constructor(public name: string, public species: string, public sound: string) {
+    // this.name = name;
+    // this.species = species;
+    // this.sound = sound;
+  }
+  public makeSound(){
+    console.log(`The ${this.name} says ${this.sound}`);
+  }
+}
+
+const dog = new Animal('German Shephard', 'dog', 'Ghew Ghew');
+const cat = new Animal('Persian', 'cat', 'meow meow');
+dog.makeSound();
+cat.makeSound();
+
+cat.name = 'special cat';
