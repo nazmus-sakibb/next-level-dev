ef3c97789e1a4866d80a2e2ca9edb4519f1ba9db
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-06 23:47:04 +0600

    static in class

---
+++ module-1/src/module-4/getter-setter.ts
@@ -0,0 +1,45 @@
+// class BankAccount {
+//     public readonly id: number;
+//     public name: string;
+//     private _balance: number;
+
+//     constructor(id: number, name: string, balance: number){
+//         this.id = id;
+//         this.name = name;
+//         this._balance = balance;
+//     }
+
+//     // getter
+//     get balance(): number {
+//         return this._balance;
+//     }
+
+//     // getBalance(): number{
+//     //     return this._balance;
+//     // }
+
+
+//     // setter
+//     set deposit(amount: number) {
+//         this._balance = this._balance + amount;
+//     }
+
+//     // addDeposit(amount: number){
+//     //     this._balance = this._balance + amount;
+//     // }
+// }
+
+// class StudentAccount extends BankAccount {
+//     test(){
+//         // this._balance
+//     }
+// }
+
+// const myAccount = new BankAccount (444, 'Persian', 40);
+// // myAccount.addDeposit(20);
+// // myAccount.getBalance();
+
+// console.log(myAccount.balance);
+
+// myAccount.deposit = 45;
+// console.log(myAccount.balance);
