a9bbbbe320b7a7772f7904ff63a9c65ea4fa7388
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-06 16:52:07 +0600

    inheritance in ts

--- module-1/src/module-4/inheritance.ts
+++ module-1/src/module-4/inheritance.ts
@@ -1,11 +1,38 @@
 class Student {
+  name: string;
+  age: number;
+  address: string;
+
+  constructor(name: string, age: number, address: string) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+
+  makeSleep(hours: number): string {
+    return `This ${this.name} will sleep for ${hours}`;
+  }
+}
+
+
+class Teacher {
     name: string;
     age: number;
     address: string;
+    designation: string;
 
-    constructor(name: string, age: number, address: string){
-        this.name = name
-        this.age = age
-        this.address = address
+    constructor(name: string, age: number, address: string, designation: string){
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.designation = designation;
+    }
+
+    makeSleep(hours: number): string {
+        return `This ${this.name} will sleep for ${hours}`;
+    }
+
+    takeClasses(NumberOfClass: number): string {
+        return `This ${this.name} will take ${NumberOfClass}`
     }
 }
