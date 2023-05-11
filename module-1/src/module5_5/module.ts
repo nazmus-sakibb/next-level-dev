dfd3ef7afbef1d3e0751544d939960c76aaf6ad3
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-11 15:19:58 +0600

    different types export import statement

--- module-1/src/module5_5/module.ts
+++ module-1/src/module5_5/module.ts
@@ -6,3 +6,15 @@ export const substract = (param1: number, param2: number): number => {
     return param1 - param2;
 } 
 
+export const multiply = (param1: number, param2: number): number => {
+    return param1 * param2;
+} 
+
+export const devide = (param1: number, param2: number): number => {
+    return param1 / param2;
+} 
+
+export const average = (param1: number, param2: number): number => {
+    return (param1 + param2) / 2;
+} 
+
