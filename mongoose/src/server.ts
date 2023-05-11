eb0ded757f4a9c3960579698611398513d30af67
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-12 03:45:35 +0600

    install express, typescript, mongoose, ts-node-dev

--- mongoose/src/server.ts
+++ mongoose/src/server.ts
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 5000;
+
+// database connection
+main().catch((err) => console.log(err));
+
+async function main() {
+  try{
+    await mongoose.connect("mongodb://127.0.0.1:27017/mongoose");
+    console.log(`Database connection successful`);
+  }
+  catch(err){
+    console.log(`Failed to connect database`, err);
+  }
+}
+
+main();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is listening on port ${port}`);
+});
