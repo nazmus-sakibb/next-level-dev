da7781a3852ead043e89b39c260a9f8e75d74dbf
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-12 13:44:17 +0600

    install and implement types

--- mongoose/src/app.ts
+++ mongoose/src/app.ts
@@ -1,13 +1,14 @@
-import express from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from 'cors';
 
-const app = express();
+const app: Application = express();
 
 // using cors
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello World");
+  next(); 
 });
 
 
--- mongoose/src/server.ts
+++ mongoose/src/server.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import app from './app';
 
-const port = 5000;
+const port: number = 5000;
 
 // database connection
 main().catch((err) => console.log(err));
