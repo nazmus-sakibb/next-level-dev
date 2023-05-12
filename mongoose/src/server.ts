7dffbcc68d0b5b7776d84354396096ba9168da71
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-12 13:08:09 +0600

    onganize code

--- mongoose/src/server.ts
+++ mongoose/src/server.ts
@@ -1,28 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
+import app from './app';
 
-const app = express();
 const port = 5000;
 
 // database connection
 main().catch((err) => console.log(err));
 
 async function main() {
-  try{
+  try {
     await mongoose.connect("mongodb://127.0.0.1:27017/mongoose");
     console.log(`Database connection successful`);
-  }
-  catch(err){
+
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (err) {
     console.log(`Failed to connect database`, err);
   }
 }
 
 main();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+
+
+
+
