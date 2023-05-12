dd7d535db9488bf9eb3d5579ef5d7cfa02aebf95
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-12 15:32:08 +0600

    creating an interface

--- mongoose/src/app.ts
+++ mongoose/src/app.ts
@@ -6,9 +6,44 @@ const app: Application = express();
 // using cors
 app.use(cors());
 
+
+// parse data
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World");
-  next(); 
+
+    // inserting a test data into mongodb
+
+    /**
+     * 1. Interface
+     * 2. Schema
+     * 3. Model
+     * 4. Database query
+     */
+
+//   res.send("Hello World");
+//   next(); 
+
+
+    // 1. crating an interface
+    interface IUser {
+        id: string;
+        role: "student";
+        password: string;
+        name: {
+            firstName: string,
+            middleName: string,
+            lastName: string,
+        };
+        dob: string;
+        gender: "male" | "female";
+        email?: string;
+        contact: string;
+        emergencyContact: string;
+        presentAddress: string;
+        permanentAddress: string;
+    }
 });
 
 
