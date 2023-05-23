6ba2ae1e06e0d8dca5d0a668120f2263c7ab3f02
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-24 00:16:10 +0600

    database connection successful

--- mongoose/src/server.ts
+++ mongoose/src/server.ts
@@ -1,34 +1,23 @@
-import mongoose from 'mongoose';
-import app from './app';
+const express = require('express');
+const mongoose = require('mongoose');
 
 
-const port: number = 5000;
+const app = express()
+const port = 5000
 
-// database connection
-main().catch((err) => console.log(err));
-
+//database connection
 async function main() {
-  try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/mongoose");
-    console.log(`Database connection successful`);
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-    app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
-  } catch (err) {
-    console.log(`Failed to connect database`, err);
-  }
-}
+  console.log(`Database connection successful`);
+};
 
 main();
 
-
 app.get('/', (req, res) => {
-  res.send("hello world");
+  res.send('Hello World!')
 })
 
-
-
-
-
-
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
