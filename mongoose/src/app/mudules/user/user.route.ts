8c8c6a25972718cde37ce82791d194e505cebaf5
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-31 14:51:04 +0600

    update repo

--- mongoose/src/app/mudules/user/user.route.ts
+++ mongoose/src/app/mudules/user/user.route.ts
@@ -2,7 +2,8 @@ import express from "express";
 import { createUser } from "./user.controller";
 const router = express.Router();
 
-router.get("/", createUser);
+router.get("/",);
+router.post('/create-user', createUser);
 
 
 export default router;
