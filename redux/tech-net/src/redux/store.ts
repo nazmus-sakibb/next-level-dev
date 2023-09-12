6004b3185da1d4ec0cc60f2aa83940eb0d55c4e7
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-09-12 17:27:04 +0600

    middleware

--- redux/tech-net/src/redux/store.ts
+++ redux/tech-net/src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/CounterSlice";
+import logger from "redux-logger";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   // devTools: true,
 });
 
