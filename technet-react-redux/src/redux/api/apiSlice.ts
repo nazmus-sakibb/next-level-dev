3eda042cda2796645a74132bd9fbe27d1e0b57a9
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-10-11 23:37:42 +0600

    initialize api layer

--- technet-react-redux/src/redux/api/apiSlice.ts
+++ technet-react-redux/src/redux/api/apiSlice.ts
@@ -5,7 +5,9 @@ export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
   endpoints: (builder) => ({
     getProducts: builder.query({
-        query: () => '/products',
+      query: () => '/products',
     }),
   }),
 });
+
+export const { useGetProductsQuery } = api;
