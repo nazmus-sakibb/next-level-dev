562cb40c63235af61cd8fbedf69b5e599545e461
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-10-11 19:15:09 +0600

    show total count on cart

--- technet-react-redux/src/redux/features/cart/cartSlice.ts
+++ technet-react-redux/src/redux/features/cart/cartSlice.ts
@@ -4,10 +4,12 @@ import { IProduct } from './../../../types/globalTypes';
 
 interface ICart {
   products: IProduct[];
+  total: number;
 }
 
 const initialState: ICart = {
   products: [],
+  total: 0,
 };
 
 const cartSlice = createSlice({
@@ -24,6 +26,8 @@ const cartSlice = createSlice({
       } else {
         state.products.push({ ...action.payload, quantity: 1 });
       }
+
+      state.total += action.payload.price;
     },
 
     removeOne: (state, action: PayloadAction<IProduct>) => {
@@ -38,6 +42,8 @@ const cartSlice = createSlice({
           (product) => product._id !== action.payload._id
         );
       }
+
+      state.total -= action.payload.price;
     },
 
     removeFromCart: (state, action: PayloadAction<IProduct>) => {
